298154abd29644761d966d66059269d4c7102eba
DC <dcposch@dcpos.ch>
2022-01-15 17:22:41 -0800

    website hello world

---
+++ website/app.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return (
+    <div>
+      <h1>hello world</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
